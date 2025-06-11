dad8f8b61881bea0816da080178f387227b66180
241019 <241019@std.hi-joho.ac.jp>
2025-06-11 10:44:07 +0900

    （`・w・）

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Hero h = new Hero();
         h.run();
+        h.test();
         SuperHero sh = new SuperHero();
+        sh.fly();
         sh.run();
+        sh.test();
     }
 }
